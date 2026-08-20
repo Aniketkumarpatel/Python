fce7072004236a43b1fe27823aed76079339a8f4
AniketKumarPatel <apatel08011@gmail.com>
2026-08-21 01:35:02 +0530

    add

---
+++ lecture_third_py/Question.py
@@ -0,0 +1,23 @@
+#WAP to ask user to enter names of their 3 favorite movies & store them in a list
+
+# movies = []
+# mov1 = (input("Enter the 1st movie:"))
+# mov2= (input("Enter the 2st movie: "))
+# mov3= (input("Enter the 3st movie: "))
+
+# movies.append(mov1)
+# movies.append(mov2)
+# movies.append(mov3)
+# print(movies)
+
+#palindrome
+
+# list1 = [2,1,3]
+# list2 = [1,2,3]
+
+# copy_list1 = list.copy()
+# copy_list1.reverse()
+
+# if(copy_list1 == list):
+#     print("palindrome")
+ 
