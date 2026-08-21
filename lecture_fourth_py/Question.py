d14141460e4eae32fcb98d4c288c3481151f7cd6
AniketKumarPatel <apatel08011@gmail.com>
2026-08-21 11:45:01 +0530

    add

---
+++ lecture_fourth_py/Question.py
@@ -0,0 +1,38 @@
+#print nums from 1 to 100
+
+# i = 1
+
+# while i <= 100 :
+#     print(i)
+#     i += 1
+
+
+
+# print the nums  from 100 to 1
+
+
+# i = 100
+
+# while i >=1 :
+#     print(i)
+#     i -= 1
+
+
+# print the multiplication table of a numbers n
+
+
+# i= 1
+# n = 5
+# while i <= 10:
+#     print(n*i)
+#     i += 1
+
+
+# print the elements of the following list using a  loop
+
+nums = [1,4,9,25,36,39,64,81,100]
+
+idx = 0
+while idx < len(nums):
+    print(nums[idx])
+    idx += 1
---
+++ lecture_fourth_py/loop.py
@@ -0,0 +1,14 @@
+# While Loop
+
+# count = 1 
+# while count <= 5 :
+#     print("hiii")
+#     count +=1
+
+
+#print nums from 1 to 5
+
+# i = 1
+# while i >= 5:
+#     print(i)
+#     i += 1 
